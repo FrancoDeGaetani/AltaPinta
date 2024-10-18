c0d385e555b6f31bd668a2b9cc4eddca4f7a7594
franco de gaetani <francodegaetani@gmail.com>
2024-10-18 07:52:25 -0300

    actualizando el html, el css, el js, agregando un a page y agregando imagenes

--- javascript/index.js
+++ javascript/index.js
@@ -3,12 +3,40 @@
 // cuanto tiempo tardaria en prepararlas para el consumidor (8 minutos por hamburguesa) y la demora del envio (15 minutos)
 
 
+// funcions
 
-// esta funcion sirve para calcular la demora de los pedidos en ser preparados y en ser enviados al consumidor
-function calculeEnvio(num1) {
-    let envio = num1 * 8 + 15
-    return envio
+function agradecer(){
+    if(hamburguesas.length === 1){
+        console.log(`Muchas gracias por confiar en nosotros, espero que disfrutes de tu pedido!`)
+    }
+    else{
+        console.log(`Muchas gracias por confiar en nosotros, espero que disfrutes de tus pedidos!`)
+    }
 }
+
+//arrays
+
+let hamburguesas = []
+
+//objetos
+
+let productos = [
+    {
+        nombre: "Simple",
+        precio: 6000,
+        demora: 6
+    },
+    {
+        nombre: "Doble",
+        precio: 8000,
+        demora: 7
+    },
+    {
+        nombre: "Triple",
+        precio: 10000,
+        demora: 9
+    }
+]
 
 //Este prompt sirve para saber que quiere hacer el cliente
 let respuestaPrompt = parseInt(prompt("Hola, soy Maria, en que puedo ayudarte? \n"+
@@ -28,30 +56,26 @@ console.log(respuestaPrompt)
 // Si el cliente se equivoca y preciona otra opcion q no sea del 1 al 4 le notificara su error, pero este no se contara dentro de los pedidos.
 if (respuestaPrompt==1){ 
     let pedido
-    let numeroDeHamburguesas = 0
-    let simple = 0
-    let doble = 0
-    let triple = 0
     do{
         pedido = parseInt(prompt('Que hamburguesas queres?\n' +'1.Simple \n' + '2.Doble \n' + '3.Triple \n'+ '4.Termina pedido'))
         console.log('Que hamburguesas queres?\n' +'1.Simple \n' + '2.Doble \n' + '3.Triple \n'+ '4.Termina pedido')
-        if(pedido==1||pedido==2||pedido==3){
-            numeroDeHamburguesas++;
-        }
         if(pedido==1){
-            simple++;
+            hamburguesas.push(productos[0]);
         }
         else if(pedido==2){
-            doble++;
+            hamburguesas.push(productos[1]);
         }
         else if(pedido==3){
-            triple++;
+            hamburguesas.push(productos[2]);;
         }
         else if (pedido > 4){
             console.log('Opcion no valida')
         }
 }while (pedido != 4)
-    console.log("El numero total de hamburguesas en el pedido es de:",numeroDeHamburguesas+ '\nSimples: '+ simple + '\nDobles: '+doble + '\nTriples: '+ triple + '\ny va a tardar alrededor de ' +  calculeEnvio(numeroDeHamburguesas) + ' minutos en llegar el pedido a tu casa. \nMuchas gracias por pedir con nosotros.');
+    console.log(`Tus hamburguesas son:`)
+    for ( let i =0 ; i < hamburguesas.length; i++ ){
+        console.log(hamburguesas[i].nombre)
+        }
 } 
 
 else if (respuestaPrompt == 2){
@@ -59,3 +83,12 @@ else if (respuestaPrompt == 2){
 
 else {
     console.log('Opncion no valida')}
+
+let precioTotal = hamburguesas.reduce ((total,hamburguesas) => total + hamburguesas.precio, 0)
+console.log (`El precio final de la compra seria $ ${precioTotal}`)
+
+let demoraTotal = hamburguesas.reduce ((demora,hamburguesas) => demora + hamburguesas.demora, 10)
+console.log (`La demora total del pedido va a ser de ${demoraTotal} minutos.`)
+
+agradecer()
+
