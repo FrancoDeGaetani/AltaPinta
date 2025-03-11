9b9d896201198225409e6752899d69356fc163fc
franco de gaetani <francodegaetani@gmail.com>
2025-03-11 16:43:33 -0300

    haciendo responmsive la pagina principla

--- javascript/index.js
+++ javascript/index.js
@@ -3,123 +3,18 @@
 //guardandolos dentro del localstorage 
 
 // Hamburguesas del Local (productos)
-let productos = [
-    {
-        id: 1,
-        nombre: "Bacon King Carne",
-        carne: "vacuna",
-        tipo:"doble",
-        precio:7000,
-        imgUrl:"../assets/img/hamburguesas/bacon-king-carne.png",
-    },
-    {
-        id: 2,
-        nombre: "Cheese Onion",
-        carne: "vacuna",
-        tipo:"doble",
-        precio:6000,
-        imgUrl:"../assets/img/hamburguesas/Cheese-Onion.png",
-    },
-    {
-        id: 3,
-        nombre: "Doble Jamon y Queso",
-        carne: "vacuna",
-        tipo:"doble",
-        precio:6500,
-        imgUrl:"../assets/img/hamburguesas/Doble-Jamon-y-Queso.png",
-    },
-    {
-        id: 4,
-        nombre: "Hamburguesa Clasica",
-        carne: "vacuna",
-        tipo:"simple",
-        precio:4000,
-        imgUrl:"../assets/img/hamburguesas/Hamburguesa-clásica.png",
-    },
-    {
-        id: 5,
-        nombre: "Melt Con Salsa Stacker",
-        carne: "vacuna",
-        tipo:"doble",
-        precio:6250,
-        imgUrl:"../assets/img/hamburguesas/Melt-con-Salsa-Stacker.png",
-    },
-    {
-        id: 6,
-        nombre: "Stacker Doble",
-        carne: "vacuna",
-        tipo:"doble",
-        precio:7000,
-        imgUrl:"../assets/img/hamburguesas/Stacker-Doble.png",
-    },
-    {
-        id: 7,
-        nombre: "Stacker Triple",
-        carne: "vacuna",
-        tipo:"triple",
-        precio:8500,
-        imgUrl:"../assets/img/hamburguesas/Stacker-Triple.png",
-    },
-    {
-        id: 8,
-        nombre: "Stacker Cuadruple",
-        carne: "vacuna",
-        tipo:"cuadruple",
-        precio:10000,
-        imgUrl:"../assets/img/hamburguesas/Stacker-Cuadruple.png",
-    },
-    {
-        id: 9,
-        nombre: "Whopper EXtreme",
-        carne: "vacuna",
-        tipo:"simple",
-        precio:6500,
-        imgUrl:"../assets/img/hamburguesas/Whopper-Extreme.png",
-    },
-    {
-        id: 10,
-        nombre: "Whopper Extreme Doble",
-        carne: "vacuna",
-        tipo:"doble",
-        precio:7500,
-        imgUrl:"../assets/img/hamburguesas/Whopper-Guacamole.png",
-    },
-    {
-        id: 11,
-        nombre: "Whopper Guacamole",
-        carne: "vacuna",
-        tipo:"doble",
-        precio:7800,
-        imgUrl:"../assets/img/hamburguesas/Whopper-Guacamole.png",
-    },
-    {
-        id: 12,
-        nombre: "Bacon King",
-        carne: "pollo",
-        tipo:"simple",
-        precio:6800,
-        imgUrl:"../assets/img/hamburguesas/Bacon-King.png",
-    },
-    {
-        id: 13,
-        nombre: "Crispy Chicken Pepino",
-        carne: "pollo",
-        tipo:"simple",
-        precio:7200,
-        imgUrl:"../assets/img/hamburguesas/Crispy-Chicken-Pepino.png",
-    },
-    {
-        id: 14,
-        nombre: "Crispy Chicken",
-        carne: "pollo",
-        tipo:"simple",
-        precio:7000,
-        imgUrl:"../assets/img/hamburguesas/Crsipy-Chicken.png",
-    },
-]
+
 
 //Variable en donde se van a estar guardando los pedidos
 const PEDIDOS =  JSON.parse(localStorage.getItem('carrito')) || [];
+
+const MENU_PARRILLA= document.getElementById('menu-parrilla')
+const MENU_POLLO = document.getElementById('menu-pollo')
+const MENU_ACOMPANAMIENTOS= document.getElementById('menu-acompañamientos')
+const MENU_BEBIDAS = document.getElementById('menu-bebidas')
+const BUSCADOR = document.getElementById('buscador')
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
     misProductos();  
@@ -152,17 +47,15 @@ async function misProductos(){
             const div = document.createElement ('div');
             div.classList.add ("producto-container");
             div.innerHTML = 
-            `
+            `               
+                            <h5 class="paty-nombre">${producto.nombre}</h5>
                             <div class="card-paty-img-container">
                                 <img src=${producto.imgUrl} alt="" class="card-paty-img">
                             </div>
-                            <div>
-                                <p class="paty-nombre">${producto.nombre}</p>
+                            <div class="precio-paty-container">
+                                <p class="paty-precio"> $ ${producto.precio}</p>
                             </div>
                             <div class="paty-precio-suma">
-                                <div class="precio-paty-container">
-                                    <p class="paty-precio"> $ ${producto.precio}</p>
-                                </div>
                                 <div class="paty-suma">
                                     <button onclick="agregarAlCarrito(${producto.id})" class="suma">Agregar Al Carrito</button>
                                     <button onclick="quitarDelCarrito(${producto.id})" class="resto">-</button>
@@ -171,11 +64,51 @@ async function misProductos(){
             `;
             divProductos.appendChild(div)
         })
+
     }catch(error){
         console.log('error')
     }
 }
 
+async function buscerProductos(){
+    const response= await fetch ('../datos.json');
+    const dataJson = await response.json();
+    const nombreProducto = dataJson.productos;
+    const divProductos = document.getElementById ("productos-container");
+
+    function mostrarProductos(productosFiltrados) {
+        divProductos.innerHTML = ""; // Limpiar el contenedor
+        productosFiltrados.forEach(producto => {
+            const div = document.createElement("div");
+            div.classList.add("producto-container");
+            div.innerHTML = `
+                            <h5 class="paty-nombre">${producto.nombre}</h5>
+                            <div class="card-paty-img-container">
+                                <img src=${producto.imgUrl} alt="" class="card-paty-img">
+                            </div>
+                            <div class="precio-paty-container">
+                                <p class="paty-precio"> $ ${producto.precio}</p>
+                            </div>
+                            <div class="paty-precio-suma">
+                                <div class="paty-suma">
+                                    <button onclick="agregarAlCarrito(${producto.id})" class="suma">Agregar Al Carrito</button>
+                                    <button onclick="quitarDelCarrito(${producto.id})" class="resto">-</button>
+                                </div>
+                            </div>
+            `;
+            contenedorProductos.appendChild(div);
+        });
+    }
+
+    mostrarProductos(nombreProducto);
+
+    // Evento para filtrar productos de la categoría "Ropa"
+    MENU_PARRILLA.addEventListener("click", () => {
+        const productosFiltrados = nombreProducto.filter(producto => producto.carne === "vacuna");
+        mostrarProductos(productosFiltrados);
+    });
+
+}
 
 //Funcion para agregar esas hamburguesas al carrito
 function agregarAlCarrito(id){
